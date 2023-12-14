9af63dcd339d211569926d4c8986443caa61875b
RovicAngelo <rovicangelolanuza9@gmail.com>
2023-12-14 16:52:16 +0800

    Added the custom table component

---
+++ WMS/src/com/lanuza/wms/ui/components/table/TableHeader.java
@@ -0,0 +1,27 @@
+package com.lanuza.wms.ui.components.table;
+
+import java.awt.Color;
+import java.awt.Font;
+import java.awt.Graphics;
+import javax.swing.JLabel;
+import javax.swing.border.EmptyBorder;
+
+public class TableHeader extends JLabel {
+	private static final long serialVersionUID = 1L;
+
+	public TableHeader(String text) {
+        super(text);
+        setOpaque(true);
+        setBackground(Color.WHITE);
+        setFont(new Font("sansserif", 1, 12));
+        setForeground(new Color(102, 102, 102));
+        setBorder(new EmptyBorder(10, 5, 10, 5));
+    }
+
+    @Override
+    protected void paintComponent(Graphics grphcs) {
+        super.paintComponent(grphcs);
+        grphcs.setColor(new Color(230, 230, 230));
+        grphcs.drawLine(0, getHeight() - 1, getWidth(), getHeight() - 1);
+    }
+}
